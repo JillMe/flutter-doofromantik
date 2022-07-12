d66646f43c93a96fb81dad1957b9f2f1b2d5d872
Jill Meyer <jill.meyer@posteo.net>
2022-07-12 13:24:24 +0200

    improve hexagon widget

--- lib/widgets/board_widget.dart
+++ lib/widgets/board_widget.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:hexagon/model/pointy_hexagon.dart';
 import 'package:hexagon/util/const.dart';
+import 'package:hexagon/widgets/triangle_piece.dart';
 
 import '../model/hex_field.dart';
 import '../model/hex_grid.dart';
@@ -43,14 +44,22 @@ class BoardWidget extends StatelessWidget {
     List<Widget> tiles = board.entries
         .map((e) => _buildPositioned(
               e.key.toPixel(),
-              HexagonWidget.fromSize(field: e.value, size: hexagonSize),
+              HexagonWidget.standard(
+                child: oldHexagons(e.value.edges),
+              ),
             ))
         .toList();
     tiles.addAll(board.availablePlaces.map((e) => _buildPositioned(
         e.toPixel(),
-        HexagonWidget.fromSize(
-          size: hexagonSize,
-          onTap: () => onTapAvailable(e),
+        HexagonWidget.standard(
+          padding: 10.0,
+          elevation: 10,
+          child: GestureDetector(
+            onTap: () => onTapAvailable(e),
+            child: Container(
+              color: Colors.red,
+            ),
+          ),
         ))));
     return tiles;
   }
@@ -60,4 +69,22 @@ class BoardWidget extends StatelessWidget {
         top: p.y - bounds.top.toDouble() - 50,
         child: child,
       );
+}
+
+Widget oldHexagons(Map<Dir, HexEdge> fields) =>
+    Stack(children: fields.entries.map(_tileForDir).toList());
+
+Widget _tileForDir(MapEntry<Dir, HexEdge> entry) {
+  return Positioned(
+      top: hexagonSize / 8,
+      left: hexagonSize / 2 - 7,
+      child: Transform.rotate(
+        angle: pi / 2.0 - pi / 3.0 * entry.key.index.toDouble(),
+        alignment: Alignment.bottomCenter,
+        child: TriangularHexagonPieceWidget(
+          width: hexagonSize,
+          height: sqrt(3) * hexagonSize / 2,
+          type: entry.value.type,
+        ),
+      ));
 }
--- lib/widgets/hexagon_widget.dart
+++ lib/widgets/hexagon_widget.dart
@@ -1,18 +1,100 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:hexagon/model/hex_field.dart';
 import 'package:hexagon/model/pointy_hexagon.dart';
-
-import 'triangle_piece.dart';
+import 'package:hexagon/util/const.dart';
 
 typedef Dir = PointyHexagonalDirection;
+
+class HexagonClipper extends CustomClipper<Path> {
+  HexagonClipper(this.pathBuilder);
+
+  final HexagonPathBuilder pathBuilder;
+
+  @override
+  Path getClip(Size size) {
+    return pathBuilder.build(size);
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    if (oldClipper is HexagonClipper) {
+      return oldClipper.pathBuilder != pathBuilder;
+    }
+    return true;
+  }
+}
+
+class HexagonPainter extends CustomPainter {
+  HexagonPainter(this.pathBuilder,
+      {this.color = Colors.white, this.elevation = 0});
+
+  final HexagonPathBuilder pathBuilder;
+  final double elevation;
+  final Color color;
+
+  final Paint _paint = Paint();
+  Path _path = Path();
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    _path = pathBuilder.build(size);
+    _paint.color = color;
+    _paint.isAntiAlias = true;
+    _paint.style = PaintingStyle.fill;
+
+    if ((elevation) > 0) {
+      canvas.drawShadow(_path, Colors.black, elevation, false);
+    }
+    canvas.drawPath(_path, _paint);
+  }
+
+  @override
+  bool hitTest(Offset position) {
+    return _path.contains(position);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return oldDelegate != this;
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is HexagonPainter &&
+          runtimeType == other.runtimeType &&
+          pathBuilder == other.pathBuilder &&
+          elevation == other.elevation &&
+          color == other.color;
+
+  @override
+  int get hashCode =>
+      pathBuilder.hashCode ^ elevation.hashCode ^ color.hashCode;
+}
+
+class HexagonPathBuilder {
+  Path build(Size size) {
+    final path = Path();
+    var padding = 0.0;
+    var height = size.height - 2 * padding;
+    var width = size.width - 2 * padding;
+    path.lineTo(width / 2 + padding, padding);
+    path.lineTo(width + padding, height / 4 + padding);
+    path.lineTo(width + padding, height * 3 / 4 + padding);
+    path.lineTo(width / 2 + padding, height + padding);
+    path.lineTo(padding, height * 3 / 4 + padding);
+    path.lineTo(padding, height / 4 + padding);
+    path.lineTo(width / 2 + padding, padding);
+    return path;
+  }
+}
 
 class CustomHexagonClip extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     final path = Path();
-    var padding = 5.0;
+    var padding = 0.0;
     var height = size.height - 2 * padding;
     var width = size.width - 2 * padding;
     path.lineTo(width / 2 + padding, padding);
@@ -32,78 +114,66 @@ class CustomHexagonClip extends CustomClipper<Path> {
 }
 
 class HexagonWidget extends StatelessWidget {
-  final HexField? field;
   final double width;
   final double height;
-  final double size;
-  final bool debug;
-  final void Function()? onTap;
+  final Widget child;
+  final double padding;
+  final double elevation;
   const HexagonWidget({
     Key? key,
-    this.field,
     required this.width,
     required this.height,
-    required this.size,
-    this.debug = false,
-    this.onTap,
+    required this.child,
+    required this.padding,
+    required this.elevation,
   }) : super(key: key);
 
-  factory HexagonWidget.fromSize(
+  factory HexagonWidget.standard(
       {Key? key,
-      HexField? field,
-      double size = 20,
-      bool debug = false,
-      void Function()? onTap}) {
+      required Widget child,
+      double elevation = 0.0,
+      double padding = 0.0}) {
     return HexagonWidget(
-        field: field,
-        width: sqrt(3) * size,
-        height: 2 * size,
-        debug: debug,
-        size: size,
+        width: sqrt(3) * hexagonSize,
+        height: 2 * hexagonSize,
         key: key,
-        onTap: onTap);
+        elevation: elevation,
+        padding: padding,
+        child: child);
   }
 
   @override
   Widget build(BuildContext context) {
-    Widget child;
-    if (field != null) {
-      if (debug) {
-        child = ClipPath(
-          clipper: CustomHexagonClip(),
-          child: Container(
-            color: Colors.green,
-          ),
-        );
-      } else {
-        child = Stack(children: field!.edges.entries.map(_tileForDir).toList());
-      }
-    } else {
-      child = ClipPath(
-        clipper: CustomHexagonClip(),
-        child: GestureDetector(
-          onTap: onTap ?? () => print("fun"),
-          child: Container(
-            color: Colors.red,
+    HexagonPathBuilder pathBuilder = HexagonPathBuilder();
+
+    return Align(
+      child: Container(
+        padding: EdgeInsets.all(padding),
+        width: width,
+        height: height,
+        child: CustomPaint(
+          painter: HexagonPainter(pathBuilder, elevation: elevation),
+          child: ClipPath(
+            clipper: HexagonClipper(pathBuilder),
+            child: OverflowBox(
+              alignment: Alignment.center,
+              maxHeight: height - padding,
+              maxWidth: width - padding,
+              child: Align(
+                alignment: Alignment.center,
+                child: child,
+              ),
+            ),
           ),
         ),
-      );
-    }
-    return SizedBox(width: width, height: height, child: child);
-  }
-
-  Widget _tileForDir(MapEntry<Dir, HexEdge> entry) {
-    return Positioned(
-        top: size / 8,
-        left: size / 2 - 7,
-        child: Transform.rotate(
-          angle: pi / 2.0 - pi / 3.0 * entry.key.index.toDouble(),
-          alignment: Alignment.bottomCenter,
-          child: TriangularHexagonPieceWidget(
-            width: size,
-            height: width / 2,
-            type: entry.value.type,
-          ),
-        ));
+      ),
+    );
+    // return SizedBox(
+    //     width: width,
+    //     height: height,
+    //     child: ClipPath(
+    //       clipper: CustomHexagonClip(),
+    //       child: child,
+    //     ));
   }
 }
